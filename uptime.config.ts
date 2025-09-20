da12457cab6fd09135a58d35b215cd156b1e12cf
Xiaozhi_z <116494985+AmbitionSight@users.noreply.github.com>
2025-09-20 21:34:02 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -34,7 +34,7 @@ const workerConfig: WorkerConfig = {
       // `method` should be a valid HTTP Method
       method: 'POST',
       // `target` is a valid URL
-      target: 'https://ambitionsight-n8n.hf.space/',
+      target: 'https://example.com',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
@@ -64,7 +64,7 @@ const workerConfig: WorkerConfig = {
       checkProxyFallback: true,
     },
     // Example TCP Monitor
-     {
+    {
       id: 'test_tcp_monitor',
       name: 'Example TCP Monitor',
       // `method` should be `TCP_PING` for tcp monitors
