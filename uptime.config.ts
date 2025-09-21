a290ed28b9970513538e1d54ad8277bdceb7ea8f
Xiaozhi_z <116494985+AmbitionSight@users.noreply.github.com>
2025-09-21 20:12:07 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,7 +2,7 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Xiaozhi_z's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/AmbitionSight', label: 'GitHub' },
